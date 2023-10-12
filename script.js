7a0685c00248f72297f7be93a8eb190926a58cf8
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-12 23:33:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const byteSize = (str) => {
-  // write your code hered
-	return bytesize(str);
+  // write your code here
+	const encoder=new TextEncoder();
+	const encodeBytes=encoder.encode(str);
+	return encodeBytes.byteLength;
+	
 };
-
-
 
 // Do not change the code below
 const str = prompt("Enter some string.");
